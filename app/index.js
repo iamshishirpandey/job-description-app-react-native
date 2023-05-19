859abdff7d574fcb7e96ccd296df8fade4bbce7d
USER <user@USERs-MacBook-Pro.local>
2023-05-19 15:43:59 +0545

    Header Layout

--- app/index.js
+++ app/index.js
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, images, icons, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: COLORS.lightWhite,
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconURL={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconURL={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
